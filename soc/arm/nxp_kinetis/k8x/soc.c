b26eda3400e47df14bcbe179422db6e43eeeb82d
Paul Vohs <joeshmoe2293@gmail.com>
2020-07-01 11:54:56 -0700

    No external clock + ADC 50 kHz (maybe broken)
    I'll push another update if the ADC is still broken for the full-on RF
    client board that comments out the parts I added.

--- soc/arm/nxp_kinetis/k8x/soc.c
+++ soc/arm/nxp_kinetis/k8x/soc.c
@@ -1,4 +1,4 @@
-/*
+ /*
  * Copyright (c) 2019 SEAL AG
  *
  * Based on NXP K6x soc.c, which is:
@@ -13,6 +13,8 @@
 #include <init.h>
 #include <fsl_common.h>
 #include <fsl_clock.h>
+#include <fsl_smc.h>
+#include <fsl_rtc.h>
 
 #define PLLFLLSEL_MCGFLLCLK	(0)
 #define PLLFLLSEL_MCGPLLCLK	(1)
@@ -28,6 +30,65 @@
 #define RUNM_RUN		(0)
 #define RUNM_VLPR		(2)
 #define RUNM_HSRUN		(3)
+
+#define BOARD_FRDIV                                     0x0U
+#define BOARD_XTAL32K_CLK_HZ                          32768U  /*!< Board RTC xtal frequency in Hz */
+#define RTC_RTC32KCLK_PERIPHERALS_ENABLED                 1U  /*!< RTC32KCLK to other peripherals: enabled */
+#define RTC_OSC_CAP_LOAD_0PF                            0x0U  /*!< RTC oscillator capacity load: 0pF */
+#define SIM_OSC32KSEL_RTC32KCLK_CLK                       2U  /*!< OSC32KSEL select: RTC32KCLK clock (32.768kHz) */
+#define SIM_RTC_CLKOUT_SEL_RTC32KCLK_CLK                  1U  /*!< RTC clock output select: RTC32KCLK clock (32.768kHz) */
+#define SIM_SDHC_CLK_SEL_CORE_SYSTEM_CLK                  0U  /*!< SDHC clock select: Core/system clock */
+#define SIM_LPUART_CLK_SEL_PLLFLLSEL_CLK                  1U  /*!< LPUART clock select: PLLFLLSEL output clock */
+#define SIM_FLEXIO_CLK_SEL_OSCERCLK_CLK                   2U  /*!< FLEXIO clock select: OSCERCLK clock */
+#define SIM_CLKOUT_SEL_FLEXBUS_CLK                        0U  /*!< CLKOUT pin clock select: FlexBus clock */
+
+/*FUNCTION**********************************************************************
+ *
+ * Function Name : CLOCK_CONFIG_SetRtcClock
+ * Description   : This function is used to configuring RTC clock including
+ * enabling RTC oscillator.
+ * Param capLoad : RTC oscillator capacity load
+ * Param enableOutPeriph : Enable (1U)/Disable (0U) clock to peripherals
+ *
+ *END**************************************************************************/
+static void CLOCK_CONFIG_SetRtcClock(uint32_t capLoad, uint8_t enableOutPeriph)
+{
+  /* RTC clock gate enable */
+  CLOCK_EnableClock(kCLOCK_Rtc0);
+  if ((RTC->CR & RTC_CR_OSCE_MASK) == 0u) { /* Only if the Rtc oscillator is not already enabled */
+    /* Set the specified capacitor configuration for the RTC oscillator */
+    RTC_SetOscCapLoad(RTC, capLoad);
+    /* Enable the RTC 32KHz oscillator */
+    RTC->CR |= RTC_CR_OSCE_MASK;
+  }
+  /* Output to other peripherals */
+  if (enableOutPeriph) {
+    RTC->CR &= ~RTC_CR_CLKO_MASK;
+  }
+  else {
+    RTC->CR |= RTC_CR_CLKO_MASK;
+  }
+  /* Set the XTAL32/RTC_CLKIN frequency based on board setting. */
+  CLOCK_SetXtal32Freq(BOARD_XTAL32K_CLK_HZ);
+  /* Set RTC_TSR if there is fault value in RTC */
+  if (RTC->SR & RTC_SR_TIF_MASK) {
+    RTC -> TSR = RTC -> TSR;
+  }
+  /* RTC clock gate disable */
+  CLOCK_DisableClock(kCLOCK_Rtc0);
+}
+
+/*FUNCTION**********************************************************************
+ *
+ * Function Name : CLOCK_CONFIG_SetFllExtRefDiv
+ * Description   : Configure FLL external reference divider (FRDIV).
+ * Param frdiv   : The value to set FRDIV.
+ *
+ *END**************************************************************************/
+static void CLOCK_CONFIG_SetFllExtRefDiv(uint8_t frdiv)
+{
+    MCG->C1 = ((MCG->C1 & ~MCG_C1_FRDIV_MASK) | MCG_C1_FRDIV(frdiv));
+}
 
 static const osc_config_t osc_config = {
 	.freq = CONFIG_OSC_XTAL0_FREQ,
@@ -60,9 +121,9 @@ static const mcg_pll_config_t pll0_config = {
 
 static const sim_clock_config_t sim_config = {
 	/* PLLFLLSEL: select PLL. */
-	.pllFllSel = PLLFLLSEL_MCGPLLCLK,
+	.pllFllSel = PERIPH_CLK_MCGIRCLK,
 	/* ERCLK32K selection: use system oscillator. */
-	.er32kSrc = ER32KSEL_OSC32KCLK,
+	.er32kSrc = SIM_OSC32KSEL_RTC32KCLK_CLK,
 	.clkdiv1 = SIM_CLKDIV1_OUTDIV1(CONFIG_K8X_CORE_CLOCK_DIVIDER - 1) |
 		   SIM_CLKDIV1_OUTDIV2(CONFIG_K8X_BUS_CLOCK_DIVIDER - 1) |
 		   SIM_CLKDIV1_OUTDIV3(CONFIG_K8X_FLEXBUS_CLOCK_DIVIDER - 1) |
@@ -73,15 +134,33 @@ static ALWAYS_INLINE void clk_init(void)
 {
 	CLOCK_SetSimSafeDivs();
 
+    /* Configure RTC clock including enabling RTC oscillator. */
+    CLOCK_CONFIG_SetRtcClock(RTC_OSC_CAP_LOAD_0PF, RTC_RTC32KCLK_PERIPHERALS_ENABLED);
+
 	CLOCK_InitOsc0(&osc_config);
 	CLOCK_SetXtal0Freq(CONFIG_OSC_XTAL0_FREQ);
 
-	CLOCK_BootToPeeMode(kMCG_OscselOsc, kMCG_PllClkSelPll0, &pll0_config);
+    /* Configure FLL external reference divider (FRDIV). */
+    CLOCK_CONFIG_SetFllExtRefDiv(BOARD_FRDIV);
+
+	CLOCK_BootToPeeMode(kMCG_OscselIrc, kMCG_PllClkSelPll0, &pll0_config);
 
 	CLOCK_SetInternalRefClkConfig(kMCG_IrclkEnable, kMCG_IrcSlow,
 				      CONFIG_MCG_FCRDIV);
 
 	CLOCK_SetSimConfig(&sim_config);
+
+    /* Set RTC_CLKOUT source. */
+    CLOCK_SetRtcClkOutClock(SIM_RTC_CLKOUT_SEL_RTC32KCLK_CLK);
+
+    /* Set SDHC clock source. */
+    CLOCK_SetSdhc0Clock(SIM_SDHC_CLK_SEL_CORE_SYSTEM_CLK);
+    /* Set LPUART clock source. */
+    CLOCK_SetLpuartClock(SIM_LPUART_CLK_SEL_PLLFLLSEL_CLK);
+    /* Set FLEXIO clock source. */
+    CLOCK_SetFlexio0Clock(SIM_FLEXIO_CLK_SEL_OSCERCLK_CLK);
+    /* Set CLKOUT source. */
+    CLOCK_SetClkOutClock(SIM_CLKOUT_SEL_FLEXBUS_CLK);
 
 	/* Divide PLL output frequency by 2 for peripherals */
 	CLOCK_SetPllFllSelClock(PLLFLLSEL_MCGPLLCLK, 1, 0);
